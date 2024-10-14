a87db0481c3336a7a35539b09d147074a928ff76
NazarHaida <nazar.haida.knm.2020@lpnu.ua>
2024-10-15 01:36:51 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,14 @@
 'use strict';
 
-const images = document.querySelector('.gallery__list')
-const mainImg = document.querySelector('#largeImg')
+const images = document.querySelector('.gallery__list');
+const mainImg = document.querySelector('#largeImg');
 
 images.addEventListener('click', (e) => {
-  e.preventDefault()
-  const imageLink = e.target.closest('.list-item__link')
-  if (imageLink) {
-    mainImg.src = imageLink.href
-  }
+  e.preventDefault();
 
-})
+  const imageLink = e.target.closest('.list-item__link');
+
+  if (imageLink) {
+    mainImg.src = imageLink.href;
+  }
+});
