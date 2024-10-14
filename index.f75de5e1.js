879b4acb413403c2a9c683af6ff7f741f7c24007
NazarHaida <nazar.haida.knm.2020@lpnu.ua>
2024-10-15 01:37:07 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const images = document.querySelector(".gallery__list");
+const mainImg = document.querySelector("#largeImg");
+images.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const imageLink = e.target.closest(".list-item__link");
+    if (imageLink) mainImg.src = imageLink.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
---
+++ index.f9628a55.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),t=document.querySelector("#largeImg");e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".list-item__link");r&&(t.src=r.href)});
+//# sourceMappingURL=index.f9628a55.js.map
